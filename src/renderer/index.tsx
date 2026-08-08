4dbc1fb0f45906f6de538f8dcfc5af46f45c2cdf
오용준 <dhdydwns723@gmail.com>
2026-08-08 19:03:06 +0900

    feat: app, router 세팅

---
+++ src/renderer/index.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../../main/global.css";
+import App from "./src/app/App";
+import { QueryClientProvider } from "@tanstack/react-query";
+import {queryClient} from "./src/app/queryClient.ts";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error('Root element "#root" was not found.');
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
