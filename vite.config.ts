36a4884943a9cd7f4370de97c3176beea3d41cb3
오용준 <dhdydwns723@gmail.com>
2026-08-10 20:40:01 +0900

    feat: svgr 활성화

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config
 export default defineConfig({
     root: resolve(__dirname, 'src/renderer/src'),
-    plugins: [react()],
+    plugins: [react(),svgr()],
 })
