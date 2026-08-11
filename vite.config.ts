5fe3e58f1a4526b5b074bf13c62875bc2a193d1e
오용준 <dhdydwns723@gmail.com>
2026-08-11 13:48:57 +0900

    refactor: tailwind 버전 업뎃

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import svgr from 'vite-plugin-svgr'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config
 export default defineConfig({
-    root: resolve(__dirname, 'src/renderer/src'),
-    plugins: [react(),svgr()],
+    root: new URL('src/renderer/src', import.meta.url).pathname,
+    publicDir: new URL('public', import.meta.url).pathname,
+    plugins: [react(), tailwindcss(), svgr()],
 })
