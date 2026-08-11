42f7a89768206e1994e445f04a7001c059567fa8
장강민 <jkm9780533@gmail.com>
2026-08-11 16:12:24 +0900

    docs: design-tokens alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tailwindcss from "@tailwindcss/vite";
+import {resolve} from "path";
 
 // https://vite.dev/config
 export default defineConfig({
   root: new URL("src/renderer/src", import.meta.url).pathname,
   publicDir: new URL("public", import.meta.url).pathname,
   plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: [
+      {
+        find: "@design-tokens",
+        replacement: resolve(__dirname, "pakages/design-tokens"),
+      },
+      {
+        find: "@/shared",
+        replacement: resolve(__dirname, "src/renderer/shared"),
+      },
+      {
+        find: "@",
+        replacement: resolve(__dirname, "src/renderer/mobile/src"),
+      },
+    ],
+  },
 });
