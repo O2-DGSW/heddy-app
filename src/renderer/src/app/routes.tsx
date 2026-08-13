388fa25c12464c688c3727340bea896df8d48fb1
장강민 <jkm9780533@gmail.com>
2026-08-13 14:46:52 +0900

    fix: 기본 라우트 /cuts 리다이렉트 재설정

--- src/renderer/src/app/routes.tsx
+++ src/renderer/src/app/routes.tsx
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { MobileLayout } from "./layouts";
+import { CutsListPage } from "../pages/cuts";
 import { RecordAddPage } from "../pages/record-add";
 import { RecordAdd2Page } from "../pages/record-add2";
 import {LoginPage} from "../pages/auth/login/ui/LoginPage.tsx";
@@ -42,6 +43,8 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<MobileLayout />}>
+        <Route path="/" element={<Navigate replace to="/cuts" />} />
+        <Route path="/cuts" element={<CutsListPage />} />
         <Route path="/cuts/add" element={<RecordAddPage />} />
         <Route path="/cuts/add2" element={<RecordAdd2Page />} />
         <Route path="*" element={<>안녕</>} />
