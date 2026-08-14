b01d6dc3aa72d1a44a8493a31740dcf794b74e7e
오용준 <dhdydwns723@gmail.com>
2026-08-14 23:51:49 +0900

    fix: 미용실 호출 로직 제거

--- src/renderer/src/features/auth/signup/index.ts
+++ src/renderer/src/features/auth/signup/index.ts
@@ -1,5 +1,3 @@
-export { SignupTypeSelect } from "./ui/SignupTypeSelect";
 export { TermsAgreement } from "./ui/TermsAgreement";
 export { CustomerAccountForm } from "./ui/CustomerAccountForm";
-export { ShopForm } from "./ui/ShopForm";
 export { useSignup } from "./model/useSignup";
