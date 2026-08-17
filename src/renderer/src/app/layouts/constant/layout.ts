2cd654642a4ab945a5f88092b56a9e5b98ff074f
오용준 <dhdydwns723@gmail.com>
2026-08-17 11:22:25 +0900

    refactor: 상수 분리

---
+++ src/renderer/src/app/layouts/constant/layout.ts
@@ -0,0 +1,9 @@
+export const BOTTOM_BAR_HIDDEN_PATHS = [
+  "/login",
+  "/signup",
+  "/welcome",
+  "/find-id",
+  "/find-password",
+];
+export const BOTTOM_BAR_HIDDEN_PREFIXES = ["/find/"];
+export const PAGE_SCROLL_PATHS = ["/cuts/add"];
